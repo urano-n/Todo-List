201777285d075004f45ff9e2870463721dd6434b
浦野なるみ <narugo0n.g@gmail.com>
2025-01-28 16:00:41 +0900

    todoアプリベース作成

---
+++ src/compornents/Form.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+const Form = ({createTodo}) => {
+  const [enterdTodo, setEnterdTodo] = useState("");
+  const addTodo = (e) => {
+    e.preventDefault();
+    const newTodo = {
+      id: Math.floor(Math.random() * 1e5),
+      content: enterdTodo,
+    };
+
+    createTodo(newTodo);
+
+    setEnterdTodo("");
+  }
+  return (
+    <div>
+      <form onSubmit={addTodo}>
+        <input type="text" value={enterdTodo} onChange={(e) => setEnterdTodo(e.target.value) }/>
+        <button onClick={addTodo}>追加</button>
+      </form>
+    </div>
+  );
+}
+
+export default Form;
